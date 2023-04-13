01594e8aecec6b253b64900ccae8f59e4111b8b2
RihpmaD <c20177@utp.edu.pe>
2023-04-12 20:16:26 -0500

    Finalizado
    Finalizado con la ayuda de Jordan Ramos

---
+++ Semana03-Ejercicio03/src/semana03/ejercicio03/Semana03Ejercicio03.java
@@ -0,0 +1,63 @@
+package semana03.ejercicio03;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author billy
+ */
+public class Semana03Ejercicio03 {
+
+    /**
+     * Determinar si los lados de un triángulo ingresado por teclado son
+     * correctos cada lado es menor a la suma de los otros dos lados.
+     *
+     * Si es que el triangulo no existe, deberá de indicarnos cual o cuales de
+     * los lados del triangulo son incorrectos.
+     *
+     * Crear un algoritmo que determine el tipo de triangulo que se ingresó por
+     * teclado: 1. Los tres lados es equilátero. 2. Dos lados es isósceles 3.
+     * Todos los lados diferentes escaleno
+     *
+     */
+    public static void main(String[] args) {
+        double lado1, lado2, lado3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\t\tSISTEMA DE MANEJO DE TRIANGULOS\n");
+        System.out.print("Ingrese el primer lado del triangulo: ");
+        lado1 = sc.nextDouble();
+        System.out.print("Ingrese el segundo lado del triangulo: ");
+        lado2 = sc.nextDouble();
+        System.out.print("Ingrese el tercer lado del triangulo: ");
+        lado3 = sc.nextDouble();
+        System.out.println("\n\tRESULTADO");
+        if (lado1 < 0 || lado2 < 0 || lado3 < 0) {
+            System.out.println("Algun valor ingresado es negativo...");
+        } else {
+            if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2) {
+                System.out.println("El triangulo existe...");
+                if (lado1 == lado2 && lado2 == lado3 && lado1 == lado3) {
+                    System.out.println("el triangulo es equilatero");
+                } else {
+                    if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+                        System.out.println("el triangulo es isosceles");
+                    } else {
+                        System.out.println("el triangulo es escaleno");
+                    }
+                }
+            } else {
+                System.out.println("El triangulo no existe...");
+                if (lado1 >= lado2 + lado3) {
+                    System.out.println("El primer lado es incorrecto");
+                }
+                if (lado2 >= lado1 + lado3) {
+                    System.out.println("El segundo lado es incorrecto");
+                }
+                if (lado3 >= lado1 + lado2) {
+                    System.out.println("El tercer lado es incorrecto");
+                }
+            }
+        }
+    }
+
+}
