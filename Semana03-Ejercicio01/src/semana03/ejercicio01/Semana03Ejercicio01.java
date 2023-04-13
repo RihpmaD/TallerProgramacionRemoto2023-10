7c72e121c4d928a5fdc2fb10a953a3af61ad3d9f
RihpmaD <c20177@utp.edu.pe>
2023-04-12 19:21:19 -0500

    Finalizado
    Se finalizo el avance

--- Semana03-Ejercicio01/src/semana03/ejercicio01/Semana03Ejercicio01.java
+++ Semana03-Ejercicio01/src/semana03/ejercicio01/Semana03Ejercicio01.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package semana03.ejercicio01;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,10 +10,25 @@ package semana03.ejercicio01;
 public class Semana03Ejercicio01 {
 
     /**
-     * @param args the command line arguments
+    Determinar si los lados de un triángulo ingresado por teclado
+    son correctos cada lado es menor a la suma de los otros
+    dos lados.
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double lado1, lado2, lado3;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\t\tSISTEMA DE MANEJO DE TRIANGULOS\n");
+        System.out.print("Ingrese el primer lado del triangulo: ");
+        lado1=sc.nextDouble();
+        System.out.print("Ingrese el segundo lado del triangulo: ");
+        lado2=sc.nextDouble();
+        System.out.print("Ingrese el tercer lado del triangulo: ");
+        lado3=sc.nextDouble();
+        System.out.println("\n\tRESULTADO");
+        if(lado1<lado2+lado3 && lado2<lado1+lado3 && lado3<lado1+lado2)
+            System.out.println("El triangulo existe...");
+        else
+            System.out.println("El triangulo no existe...");
     }
     
 }
