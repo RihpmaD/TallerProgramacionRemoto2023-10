df03d15f2b708562411f9b5fa048bf52ee7b3abf
RihpmaD <c20177@utp.edu.pe>
2023-05-03 19:36:26 -0500

    Finalizado
    En clases

---
+++ Semana06Ejercicio01/src/semana06ejercicio01/Semana06Ejercicio01.java
@@ -0,0 +1,26 @@
+
+package semana06ejercicio01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author billy
+ */
+public class Semana06Ejercicio01 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int numero, suma=0, cantidad=0;
+        System.out.println("\t\tSISTEMA DE INGRESO DE NUMEROS\n");
+        while(cantidad<5){
+            System.out.print("Ingrese numero "+cantidad+": ");
+            numero=sc.nextInt();
+            suma+=numero;
+            cantidad++;
+        }
+        System.out.println("\n\tRESULTADOS");
+        System.out.println("La suma de los numeros es: "+suma);
+    }
+    
+}
