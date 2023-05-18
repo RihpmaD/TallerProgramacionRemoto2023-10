b3f4e98e6d8b3a3e9be514f5856b8bc9bca68147
RihpmaD <c20177@utp.edu.pe>
2023-05-17 19:53:50 -0500

    Actualizado
    Finalizado

--- Semana08Ejercicio01/src/semana08ejercicio01/Principal.java
+++ Semana08Ejercicio01/src/semana08ejercicio01/Principal.java
@@ -1,4 +1,3 @@
-
 package semana08ejercicio01;
 
 import java.util.Scanner;
@@ -12,24 +11,31 @@ public class Principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
-        double sum=0;
+        double sum = 0;
         System.out.println("\n\t\tSISTEMA DE SERIES\n");
-        do{
+        do {
             System.out.print("Ingrese la cantidad de elementos: ");
-            n=sc.nextInt();
-            if(n<=0)
+            n = sc.nextInt();
+            if (n <= 0) {
                 System.out.println("El valor ingresado es incorrecto...");
-        }while(n<=0);
+            }
+        } while (n <= 0);
         //crear un break que salga de bucle cuando la suma sea superior 
-        //al valor de i elevado al cuadrado.
-        for(int i=1;i<=n;i++){
-            sum+=Math.pow(i, i);
-            if(i!=n)
-                System.out.print(Math.pow(i, i)+" + ");
-            else
-                System.out.print(Math.pow(i, i)+" = ");
+        //al valor de n elevado al cuadrado.
+        for (int i = 1; i <= n; i++) {
+            sum += Math.pow(i, i);
+            if (i != n) {
+                if (sum > n * n) {
+                    System.out.print(Math.pow(i, i) + " = ");
+                    break;
+                }
+                System.out.print(Math.pow(i, i) + " + ");
+            } else {
+                System.out.print(Math.pow(i, i) + " = ");
+            }
+
         }
-        System.out.println(sum+"\n");
+        System.out.println(sum + "\n");
     }
-    
+
 }
