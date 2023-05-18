61e7c8caf14796a8f27b7971bf94af6f4850b535
RihpmaD <c20177@utp.edu.pe>
2023-05-17 19:37:15 -0500

    Ejercicio 01
    Finalizado

---
+++ Semana08Ejercicio01/src/semana08ejercicio01/Principal.java
@@ -0,0 +1,35 @@
+
+package semana08ejercicio01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author billy
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        double sum=0;
+        System.out.println("\n\t\tSISTEMA DE SERIES\n");
+        do{
+            System.out.print("Ingrese la cantidad de elementos: ");
+            n=sc.nextInt();
+            if(n<=0)
+                System.out.println("El valor ingresado es incorrecto...");
+        }while(n<=0);
+        //crear un break que salga de bucle cuando la suma sea superior 
+        //al valor de i elevado al cuadrado.
+        for(int i=1;i<=n;i++){
+            sum+=Math.pow(i, i);
+            if(i!=n)
+                System.out.print(Math.pow(i, i)+" + ");
+            else
+                System.out.print(Math.pow(i, i)+" = ");
+        }
+        System.out.println(sum+"\n");
+    }
+    
+}
