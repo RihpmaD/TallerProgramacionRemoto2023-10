ccbd1d5ec65f2fcf6c8044089fe6c2c50b71fd17
RihpmaD <c20177@utp.edu.pe>
2023-06-21 20:27:16 -0500

    Avance 2
    Actualización

--- Semana13Ejercicio01/src/semana13ejercicio01/Empleado.java
+++ Semana13Ejercicio01/src/semana13ejercicio01/Empleado.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package semana13ejercicio01;
 
 /**
@@ -9,5 +6,47 @@ package semana13ejercicio01;
  * @author billy
  */
 public class Empleado {
+    private String nombre;//Apellidos y nombres
+    private String dni;
+    private int edad;//De 18-45 años
+    private boolean casado;
+    private double salario;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String dni, int edad, 
+            boolean casado, double salario) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+        this.casado = casado;
+        this.salario = salario;
+    }
+    
+    public void clasificacion(){
+        if(this.edad<=21)
+            System.out.println("Principiante...");
+        else
+            if(this.edad<=35)
+                System.out.println("Intermedio...");
+            else
+                System.out.println("Senior...");
+    }
+    
+    public void incrementarSalario(int porcentaje){
+        this.salario*=(porcentaje/100.00);
+        System.out.println("Incremento efectuado...");
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "\n\tnombre=" + nombre + 
+                "\n\tdni=" + dni + 
+                "\n\tedad=" + edad + 
+                "\n\tcasado=" + casado + 
+                "\n\tsalario=" + salario +"\n\t\t"+ '}';
+    }
+    
     
 }
--- Semana13Ejercicio01/src/semana13ejercicio01/Principal.java
+++ Semana13Ejercicio01/src/semana13ejercicio01/Principal.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package semana13ejercicio01;
 
 /**
@@ -10,11 +7,8 @@ package semana13ejercicio01;
  */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
