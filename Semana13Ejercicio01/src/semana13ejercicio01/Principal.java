77bdff6219c6b032862facdcc071cb5162ab7e57
RihpmaD <c20177@utp.edu.pe>
2023-06-21 20:14:36 -0500

    Avance 1
    Actualizacion

---
+++ Semana13Ejercicio01/src/semana13ejercicio01/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana13ejercicio01;
+
+/**
+ *
+ * @author billy
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
